2f74df24f7f6ddc54270c45b837f32b8e33bbb40
Thomaz Barros <thomazavila@gmail.com>
2016-04-30 05:02:41 -0300

    Creating class Contatct.

---
+++ Contacts/Contacts/Contact.h
@@ -0,0 +1,13 @@
+//
+//  Contact.h
+//  Contacts
+//
+//  Created by Thomaz Avila Barros on 30/04/16.
+//  Copyright © 2016 ThomazBarros. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Contact : NSObject
+
+@end
---
+++ Contacts/Contacts/Contact.m
@@ -0,0 +1,13 @@
+//
+//  Contact.m
+//  Contacts
+//
+//  Created by Thomaz Avila Barros on 30/04/16.
+//  Copyright © 2016 ThomazBarros. All rights reserved.
+//
+
+#import "Contact.h"
+
+@implementation Contact
+
+@end
