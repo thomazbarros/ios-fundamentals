9b1ebf035763bd43cb46097523abe7d63206e2ba
Thomaz Barros <thomazavila@gmail.com>
2016-05-01 06:34:05 -0300

    TestingIntegrationBetweenObjectiveCAndSwift created.

---
+++ TestingIntegrationBetweenObjectiveCAndSwift/TestingIntegrationBetweenObjectiveCAndSwift/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  TestingIntegrationBetweenObjectiveCAndSwift
+//
+//  Created by Thomaz Avila Barros on 01/05/16.
+//  Copyright © 2016 ThomazBarros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ TestingIntegrationBetweenObjectiveCAndSwift/TestingIntegrationBetweenObjectiveCAndSwift/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestingIntegrationBetweenObjectiveCAndSwift
+//
+//  Created by Thomaz Avila Barros on 01/05/16.
+//  Copyright © 2016 ThomazBarros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
