cd14efefc526eb5a53fdfba08d42ce20bad86ae9
Thomaz Barros <thomazavila@gmail.com>
2016-05-01 04:36:06 -0300

    Testing integration between Swift and Objective-C. Also removing first project regarding this test. I decided to take another approach.

---
+++ TestingIntegrationBetweenSwiftAndObjectiveC/TestingIntegrationBetweenSwiftAndObjectiveC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestingIntegrationBetweenSwiftAndObjectiveC
+//
+//  Created by Thomaz Avila Barros on 01/05/16.
+//  Copyright © 2016 ThomazBarros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
