6394529a116caf921227482f455df6d11ce0c6d8
Thomaz Barros <thomazavila@gmail.com>
2016-05-05 03:59:15 -0300

    Starting to create a ViewController just with Objective-C classes.

---
+++ Contacts/Contacts/ContactsListViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContactsListViewController.h
+//  Contacts
+//
+//  Created by Thomaz Avila Barros on 05/05/16.
+//  Copyright © 2016 ThomazBarros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactsListViewController : UITableViewController
+
+@end
---
+++ Contacts/Contacts/ContactsListViewController.m
@@ -0,0 +1,13 @@
+//
+//  ContactsListViewController.m
+//  Contacts
+//
+//  Created by Thomaz Avila Barros on 05/05/16.
+//  Copyright © 2016 ThomazBarros. All rights reserved.
+//
+
+#import "ContactsListViewController.h"
+
+@implementation ContactsListViewController
+
+@end
