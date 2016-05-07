842b7290ce9846dc31b02f952b48927b9e5ec01c
Thomaz Barros <thomazavila@gmail.com>
2016-05-07 17:17:07 -0300

    [Contacts] Setting up the UINavigationBar.

--- Contacts/Contacts/ContactsListViewController.m
+++ Contacts/Contacts/ContactsListViewController.m
@@ -10,4 +10,18 @@
 
 @implementation ContactsListViewController
 
+- (id)init{
+    self = [super init];
+    if (self) {
+        self.navigationItem.title = @"Contacts";
+        UIBarButtonItem *showFormButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(showForm)];
+        self.navigationItem.rightBarButtonItem = showFormButton;
+    }
+    return self;
+}
+
+- (void)showForm{
+    NSLog(@"This is a test. The form is going to be executed by this method.");
+}
+
 @end
