238b4bf1b660ba16976214de17d8a3c937433524
Thomaz Barros <thomazavila@gmail.com>
2016-09-24 11:28:08 -0300

    [Dog Age Calculator] Setting up the UI.

--- Dog Age Calculator/Dog Age Calculator/ViewController.swift
+++ Dog Age Calculator/Dog Age Calculator/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("The app has been loaded.")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
